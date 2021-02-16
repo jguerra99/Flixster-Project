1a26ad6a06853c2fceb69b9e9eb3d06af5d265de
jose guerra <josexjulian@hotmail.com>
2021-02-16 17:47:48 -0500

    Initial Commit

---
+++ Flixter_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixter_Project
+//
+//  Created by Jose Lopez on 2/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
